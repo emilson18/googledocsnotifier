4bdea20dc4223d4733eb1b550618caf00a472da6
gclin009 <gclin009@147feabf-7d7a-810a-69be-0049871b7931>
2010-05-20 12:02:45 +0000

    Change the file name from Form1.cs to MainWindow.cs.

--- GoogleDocsNotifier/Program.cs
+++ GoogleDocsNotifier/Program.cs
@@ -15,7 +15,7 @@ namespace GoogleDocsNotifier
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
